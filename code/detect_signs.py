4701cf7a3294f9f52aab49b3406f3440ce44eda9
Patrick Schlosser <pschloss@nd.edu>
2025-03-03 17:07:52 -0500

    update

--- code/detect_signs.py
+++ code/detect_signs.py
@@ -78,23 +78,23 @@ def detect_sign_new(cam, model):
 
 if __name__ == '__main__':
     # Initialize Picamera2
-    print("Configure camera...")
-    picam2 = Picamera2()
-    config = picam2.create_still_configuration(
-        main={"size": (640, 480), "format": "RGB888"},  # Small but clear resolution, RGB for ML models
-        lores={"size": (320, 240)},  # Optional low-res for faster preview if needed
-        display="lores"  # Use low-res for previewing (if using)
-    )
-    picam2.configure(config)
-    picam2.start()
-    # load model
-    print('Loading YOLO model...')
-    version = 'yolo11n.tflite'
-    model = YOLO(version)
-    print('Model loaded!')
-
+    # print("Configure camera...")
+    # picam2 = Picamera2()
+    # config = picam2.create_still_configuration(
+    #     main={"size": (640, 480), "format": "RGB888"},  # Small but clear resolution, RGB for ML models
+    #     lores={"size": (320, 240)},  # Optional low-res for faster preview if needed
+    #     display="lores"  # Use low-res for previewing (if using)
+    # )
+    # picam2.configure(config)
+    # picam2.start()
+    # # load model
+    # print('Loading YOLO model...')
+    # version = 'yolo11n.tflite'
+    # model = YOLO(version)
+    # print('Model loaded!')
+    cam, model = initialize()
     while 1:
-        d_class = detect_sign_new(model,picam2)
+        d_class = detect_sign_new(model,cam)
         print(d_class)
         # color_print('Analyzing picture',"cyan")
         # frame = picam2.capture_array()
