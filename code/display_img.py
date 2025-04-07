e0321eff8d6edbcc40b99946c8ae4f7d2dbe55d7
Patrick Schlosser <pschloss@nd.edu>
2025-04-07 17:24:59 -0400

    live image function

---
+++ code/display_img.py
@@ -0,0 +1,18 @@
+from tft_display import TFTDisplay
+from picamera2 import Picamera2
+
+screen = TFTDisplay()
+picam2 = Picamera2()
+config = picam2.create_still_configuration(
+    main={
+        "size": (640, 480),
+        "format": "RGB888",
+    },  # Small but clear resolution, RGB for ML models
+)
+picam2.configure(config)
+picam2.start()
+
+img = picam2.capture_array()
+
+screen.clear_screen("black")
+screen.display_bmp_image(img, position=(0, 0))
